c01d371279574568fed029a02069550191ac0d83
Farshad Nabizadeh <Farshadn@hotelistan.com>
2023-05-18 06:08:29 -0700

    master

--- assets/javascriptFiles/main.js
+++ assets/javascriptFiles/main.js
@@ -6,6 +6,6 @@ const starter = () => {
     $('#changingStatusIndex>li:nth-child(1)>a>div:nth-child(3)').addClass('status')
 }
 function changingStatus(object) {
-    $('#changingStatusIndex>li>div:nth-child(3)').removeClass('status')
-    $(`#${object.id}>div:nth-child(3)`).addClass('status')
+    $('#changingStatusIndex>li>a>div:nth-child(3)').removeClass('status')
+    $(`#${object.id}>a>div:nth-child(3)`).addClass('status')
 }
